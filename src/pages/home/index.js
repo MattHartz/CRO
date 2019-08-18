d12dd0867c0bf771173850c28f2254b72af4f05a
Matthew Hartz <matthew.vincent.hartz@gmail.com>
2019-08-17 20:10:32 -0700

    hid some content

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -23,19 +23,19 @@ function HomePage(props) {
           router.push("/pricing");
         }}
       />
-      <ClientsSection color="light" size="normal" title="" subtitle="" />
-      <FeaturesSection
+      {/*<ClientsSection color="light" size="normal" title="" subtitle="" />*/}
+      {/*<FeaturesSection
         color="white"
         size="medium"
         title="Features"
         subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud."
-      />
-      <TestimonialsSection
+      />*/}
+      {/*<TestimonialsSection
         color="light"
         size="medium"
         title="Here's what people are saying"
         subtitle=""
-      />
+      />*/}
       <NewsletterSection
         color="white"
         size="medium"
